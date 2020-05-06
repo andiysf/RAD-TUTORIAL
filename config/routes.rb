1d3181685c619303af44a6bade56109bdfdce66b
Rapid <muhammad.andiyusuf@ymail.com>
2020-05-06 22:18:19 +1000

    RAD-TUTORIAL

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users
+  get 'users/new'
+
+  #get 'static_pages/home'
+  root 'static_pages#home'
+  #get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+  #get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+  #get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
