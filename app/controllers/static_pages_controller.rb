a11ea57d91348e4bdcdff4407136dac7afbb8617
RAD <s3827507@student.rmit.edu.au>
2020-05-28 20:25:19 +1000

    RAD-TUTORIAL

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @microposts = Micropost.order('created_at DESC').paginate(:page => params[:page])
-    @top_five = Micropost.where(updated_at: 48.hours.ago..DateTime.now).order('created_at 	DESC').first(5)
+    @top_five = Micropost.where(updated_at: 72.hours.ago..DateTime.now).order('created_at DESC').first(5)
   end
 
   def help
