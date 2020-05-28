7aa7740ddbb0dc1d89b6d9e309d61d1a499022a0
RAD <s3827507@student.rmit.edu.au>
2020-05-28 13:58:25 +1000

    RAD-TUTORIAL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  host = 'example.com'
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'https://polar-scrubland-72622.herokuapp.com/'
 
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
